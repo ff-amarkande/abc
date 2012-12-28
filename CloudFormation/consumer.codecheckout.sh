0474dcb56f8340660c4016784de1d96fab0c6332
Stephen Harrison <stephen@firstfuel.com>
2012-12-27 22:42:04 -0500

    Tweaks. Replace overwritten backend deployment script.

--- CloudFormation/consumer.codecheckout.sh
+++ CloudFormation/consumer.codecheckout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-#  Admin portal checkout script
+#  Comsumer portal checkout script
 #
 
 # Here's where we're going to put the application
@@ -13,8 +13,8 @@ cat /mnt/ssh_keys >> ~ec2-user/.ssh/authorized_keys
 
 # Move the configuration files into place
 #
-cp /mnt/database.yml $root/admin/config
-cp /mnt/settings.yml $root/admin/config
+cp /mnt/database.yml $root/customer/config
+cp /mnt/settings.yml $root/customer/config
 
 # Link the application to main, the common Rails application root
 #
@@ -24,6 +24,8 @@ ln -s $root/customer $root/main
 #
 chown -R ec2-user:ec2-user $root
 
-# Fire up the Bertie!
+# Copy the nginx conf file and fire up the Bertie!
 #
+cp /mnt/nginx.conf /opt/nginx/conf
+
 /etc/init.d/nginx restart 
